b089c4dbd9fc850c93ca7c6dd6474eb993e161cc
RobelGebrekidan2 <146842620+RobelGebrekidan2@users.noreply.github.com>
2024-01-25 17:02:06 +0300

    first commit

---
+++ rvs-netflix-clone-2024/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>Thank you Heavenly father for all the blessings you've bestowed upon me and my loved ones.</h1>
+    </div>
+  );
+}
+
+export default App;
